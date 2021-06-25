55aff21645971a4b7e99a0529760673480e1891a
Kurt <chrisronsmith@gmail.com>
2021-06-25 14:02:41 -0400

    Improve layout

--- App.tsx
+++ App.tsx
@@ -1,33 +1,101 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-// import { StatusBar } from 'expo-status-bar'
+import React, { useState } from "react";
+import {
+  Button,
+  NativeSyntheticEvent,
+  NativeTouchEvent,
+  Text,
+  View,
+} from "react-native";
 
-export default function App() {
-  const [count, setCount] = useState(0)
-
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count -1)
-  const reset = () => setCount(0)
-
+export default () => {
   return (
-    <View style={styles.container}>
-      {/* <StatusBar style="auto" /> */}
-      <Text >{count}</Text>
-      <Button  title="increment" onPress={ increment }>Increment Count</Button>
-      <Button title="decrement" onPress={ decrement }>Decrement Count</Button>
-      <Button title="reset" onPress={ reset }>Reset Count</Button>
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <Counter />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  bigFont: {
-    fontSize: 50
-  }
-});
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+      }}
+    >
+      <CounterDisplay count={count} />
+      <CounterButtons
+        increment={increment}
+        decrement={decrement}
+        reset={reset}
+      />
+    </View>
+  );
+};
+
+const CounterButtons: React.FC<{
+  increment: () => void;
+  decrement: () => void;
+  reset: () => void;
+}> = ({
+  increment,
+  decrement,
+  reset,
+}) => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-around",
+        width: "80%",
+      }}
+    >
+      <CounterButton title="decrement" onPress={decrement} />
+      <CounterButton title="reset" onPress={reset} />
+      <CounterButton title="increment" onPress={increment} />
+    </View>
+  );
+};
+
+const CounterDisplay: React.FC<{
+  count: number;
+}> = ({ count }) => (
+  <View
+    style={{
+      flex: 2,
+      alignItems: "center",
+      justifyContent: "center",
+    }}
+  >
+    <Text
+      style={{
+        fontSize: 200,
+        textShadowColor: "#999",
+        textShadowRadius: 4,
+        textShadowOffset: { width: 2, height: 2 },
+      }}
+    >
+      {count}
+    </Text>
+  </View>
+);
+
+const CounterButton: React.FC<{
+  onPress: (ev: NativeSyntheticEvent<NativeTouchEvent>) => void;
+  title: string;
+}> = ({ onPress, title }) => (
+  <View style={{ flex: 1, maxWidth: "30%" }}>
+    <Button title={title} onPress={onPress} />
+  </View>
+);
